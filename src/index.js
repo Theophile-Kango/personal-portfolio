d00727f34c233fd58a9066d849589d5b4ee64d49
Theophile-Kango <fadhili.kango@gmail.com>
2020-06-19 13:35:49 +0200

    Add link to my address mamail

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
 import './style.scss';
 
 
