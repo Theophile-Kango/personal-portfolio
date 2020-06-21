1b71d591d77cdc538450b4f2d1b5e1d85efb7faa
Theophile-Kango <fadhili.kango@gmail.com>
2020-06-21 07:07:51 +0200

    Refactoring the code

--- src/event.js
+++ src/event.js
@@ -5,20 +5,47 @@ export const event = () => {
 
     const mainSection = document.querySelector('.main-section');
     const details = document.querySelector('.details');
+    const portfolio = document.querySelector('#portfolio');
+    const projects = document.querySelector('#projects');
+    const skills = document.querySelector('#skills');
+    const resume = document.querySelector('#resume');
 
     //const dNone = document.querySelector('.d-none');
     menu.querySelectorAll('.action').forEach(element => {
         element.addEventListener('click', () => {
+            if (element.textContent == "Projects"){
+                classEffect([projects, skills, resume], 'active')
+            }else if(element.textContent == "Skills"){
+                classEffect([skills, projects, resume], 'active');
+            }else if(element.textContent == "Resume"){
+                classEffect([resume, skills, projects], 'active');
+            }
             header.classList.remove('d-none');
             mainSection.classList.add('d-none');
             details.classList.add('d-none');
         })
     });
 
-    const portfolio = document.querySelector('#portfolio');
     portfolio.addEventListener('click', () => {
-        header.classList.add('d-none');
-        mainSection.classList.remove('d-none');
-        details.classList.remove('d-none');
-    })
+        classEffect([portfolio, mainSection, details], 'd-none');
+    });
+
+    projects.addEventListener('click', () => {
+        classEffect([projects, skills, resume], 'active');
+    });
+
+    skills.addEventListener('click', () => {
+        classEffect([skills, projects, resume], 'active');
+    });
+
+    resume.addEventListener('click', () => {
+        classEffect([resume, skills, projects], 'active');
+    });
+
+}
+
+const classEffect = (addRemove, name) => {
+    const [first, ...last] = addRemove;
+    first.classList.add(name);
+    last.forEach(elt => elt.classList.remove(name));
 }
